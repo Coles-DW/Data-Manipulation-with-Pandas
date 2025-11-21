fa9eaa0bc67a6c04444a918a0b5d957fe5d94571
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2025-11-21 15:51:27 -0700

    Update summary statistics.py

--- summary statistics.py
+++ summary statistics.py
@@ -2,3 +2,50 @@ Mean and median
 Summary statistics are exactly what they sound like - they summarize many numbers in one statistic. For example, mean, median, minimum, maximum, and standard deviation are summary statistics. Calculating summary statistics allows you to get a better sense of your data, even if there's a lot of it.
 
 sales is available and pandas is loaded as pd.
+
+# Print the head of the sales DataFrame
+print(sales.head())
+
+# Print the info about the sales DataFrame
+print(sales.info())
+
+# Print the mean of weekly_sales
+print(sales["weekly_sales"].mean())
+
+# Print the median of weekly_sales
+print(sales["weekly_sales"].median())
+
+# Print the maximum of the date column
+print(sales["date"].max())
+
+# Print the minimum of the date column
+print(sales["date"].min())
+
+
+## Efficient summaries
+While pandas and NumPy have tons of functions, sometimes, you may need a different function to summarize your data.
+
+The .agg() method allows you to apply your own custom functions to a DataFrame, as well as apply functions to more than one column of a DataFrame at once, making your aggregations super-efficient. For example,
+
+df['column'].agg(function)
+In the custom function for this exercise, "IQR" is short for inter-quartile range, which is the 75th percentile minus the 25th percentile. It's an alternative to standard deviation that is helpful if your data contains outliers.
+
+sales is available and pandas is loaded as pd.
+
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+    
+# Print IQR of the temperature_c column
+print(sales["temperature_c"].agg(iqr))
+
+# Update to print IQR of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg(iqr))
+
+# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr, "median"]))
+
+                                                                                 Cumulative statistics
+Cumulative statistics can also be helpful in tracking summary statistics over time. In this exercise, you'll calculate the cumulative sum and cumulative max of a department's weekly sales, which will allow you to identify what the total sales were so far as well as what the highest weekly sales were so far.
+
+A DataFrame called sales_1_1 has been created for you, which contains the sales data for department 1 of store 1. pandas is loaded as pd.
