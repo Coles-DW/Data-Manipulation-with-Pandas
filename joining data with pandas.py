02e2ca2d2dfc4a0c109a01e0122db17a91b75ba5
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2025-11-24 06:06:41 -0700

    Update joining data with pandas.py

--- joining data with pandas.py
+++ joining data with pandas.py
@@ -170,3 +170,91 @@ sorted_pop_vac_lic = pop_vac_lic.sort_values(["vacant", "account", "pop_2010"],
 print(sorted_pop_vac_lic.head())
 
 You merged multiple tables with varying relationships and added suffixes to make your column names clearer. Using your skills, you were able to pull together information from different tables to see that the 7th ward would be a good place to build your goat farm!
+
+Counting missing rows with left join
+The Movie Database is supported by volunteers going out into the world, collecting data, and entering it into the database. This includes financial data, such as movie budget and revenue. If you wanted to know which movies are still missing data, you could use a left join to identify them. Practice using a left join by merging the movies table and the financials table.
+
+The movies and financials tables have been loaded for you.
+
+# Merge movies and financials with a left join
+movies_financials = movies.merge(financials, on='id', how='left')
+
+print(movies_financials.head())
+print(movies_financials.shape)
+
+# Merge the movies table with the financials table with a left join
+movies_financials = movies.merge(financials, on='id', how='left')
+
+# Count the number of rows in the budget column that are missing
+number_of_missing_fin = movies_financials['budget'].isnull().sum()
+
+# Print the number of movies missing financials
+print(number_of_missing_fin)
+
+Enriching a dataset
+Setting how='left' with the .merge()method is a useful technique for enriching or enhancing a dataset with additional information from a different table. In this exercise, you will start off with a sample of movie data from the movie series Toy Story. Your goal is to enrich this data by adding the marketing tag line for each movie. You will compare the results of a left join versus an inner join.
+
+The toy_story DataFrame contains the Toy Story movies. The toy_story and taglines DataFrames have been loaded for you.
+
+# Merge the toy_story and taglines tables with a left join
+toystory_tag = toy_story.merge(taglines, on='id', how='left')
+
+# Print the rows and shape of toystory_tag
+print(toystory_tag)
+print(toystory_tag.shape)
+
+# Merge the toy_story and taglines tables with a inner join
+toystory_tag = toy_story.merge(taglines, on='id')
+
+# Print the rows and shape of toystory_tag
+print(toystory_tag)
+print(toystory_tag.shape)
+
+How many rows with a left join?
+Select the true statement about left joins.
+
+Try running the following code statements:
+
+left_table.merge(one_to_one, on='id', how='left').shape
+left_table.merge(one_to_many, on='id', how='left').shape
+Note that the left_table starts out with 4 rows.
+
+Right join to find unique movies
+Most of the recent big-budget science fiction movies can also be classified as action movies. You are given a table of science fiction movies called scifi_movies and another table of action movies called action_movies. Your goal is to find which movies are considered only science fiction movies. Once you have this table, you can merge the movies table in to see the movie names. Since this exercise is related to science fiction movies, use a right join as your superhero power to solve this problem.
+
+The movies, scifi_movies, and action_movies tables have been loaded for you.
+
+# Merge action_movies to scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right')
+
+# Merge action_movies to scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+                                   suffixes= ['_act', '_sci'])
+
+# Print the first few rows of action_scifi to see the structure
+print(action_scifi.head())
+
+# Merge action_movies to the scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+                                   suffixes=('_act','_sci'))
+
+# From action_scifi, select only the rows where the genre_act column is null
+scifi_only = action_scifi[action_scifi["genre_act"].isnull()]
+
+print(scifi_only)
+
+# Merge action_movies to the scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+                                   suffixes=('_act','_sci'))
+
+# From action_scifi, select only the rows where the genre_act column is null
+scifi_only = action_scifi[action_scifi['genre_act'].isnull()]
+
+# Merge the movies and scifi_only tables with an inner join
+movies_and_scifi_only = movies.merge(scifi_only, left_on='id', right_on='movie_id')
+
+# Print the first few rows and shape of movies_and_scifi_only
+print(movies_and_scifi_only.head())
+print(movies_and_scifi_only.shape)
+
+you found over 250 action only movies by merging action_movies and scifi_movies using a right join. With this, you were able to find the rows not found in the action_movies table. Additionally, you used the left_on and right_on arguments to merge in the movies table. 
