b28373cb09510f589213581ea98fd4fa1d207bdd
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2025-11-30 15:35:40 -0700

    Update joining data with pandas.py

--- joining data with pandas.py
+++ joining data with pandas.py
@@ -667,3 +667,31 @@ ur_sorted = ur_tall.sort_values(by='date')
 # Plot the unempl_rate by date
 ur_sorted.plot(x='date', y='unempl_rate')
 plt.show()
+
+Using .melt() for stocks vs bond performance
+It is widespread knowledge that the price of bonds is inversely related to the price of stocks. In this last exercise, you'll review many of the topics in this chapter to confirm this. You have been given a table of percent change of the US 10-year treasury bond price. It is in a wide format where there is a separate column for each year. You will need to use the .melt() method to reshape this table.
+
+Additionally, you will use the .query() method to filter out unneeded data. You will merge this table with a table of the percent change of the Dow Jones Industrial stock index price. Finally, you will plot data.
+
+The tables ten_yr and dji have been loaded for you.
+
+ten_yr.head()
+
+# Use melt on ten_yr, unpivot everything besides the metric column
+bond_perc = ten_yr.melt(id_vars='metric', var_name='date', value_name='close')
+
+bond_perc.head()
+
+# Use query on bond_perc to select only the rows where metric=close
+bond_perc_close = bond_perc.query('metric=="close"')
+
+bond_perc_close.head()
+
+# Merge (ordered) dji and bond_perc_close on date with an inner join
+dow_bond = pd.merge_ordered(dji, bond_perc_close, on='date', how='inner', suffixes=('_dow', '_bond'))
+
+dow_bond.head()
+
+# Plot only the close_dow and close_bond columns
+dow_bond.plot(y=["close_dow", "close_bond"], x='date', rot=90)
+plt.show()
